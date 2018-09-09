8d533b353e427953e600e49b65c4c5a9d6896699
Sherry <th3bomberry@hotmail.com>
2018-09-09 16:00:23 +0800

    Update easter_egg.js

--- easter_egg.js
+++ easter_egg.js
@@ -1,4 +1,4 @@
-```
+
 var deg = 0;
 setInterval(function() {
 document.getElementsByTagName("body")[0].style.background = "black";
@@ -10,4 +10,4 @@ setInterval(function() {
 deg ++;
 document.getElementsByTagName("body")[0].style.transform = "rotate("+deg+"deg)";
 }, 200);
-```
+
